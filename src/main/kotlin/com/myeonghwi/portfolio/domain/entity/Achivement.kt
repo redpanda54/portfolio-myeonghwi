0eed9a62ce44fe25e452f906fa3967d972b68ff4
Kim Myeong Hwi <103114227+redpanda54@users.noreply.github.com>
2025-03-06 23:27:07 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/myeonghwi/portfolio/domain/entity/Achivement.kt
+++ src/main/kotlin/com/myeonghwi/portfolio/domain/entity/Achivement.kt
@@ -1,14 +1,29 @@
 package com.myeonghwi.portfolio.domain.entity
 
 import jakarta.persistence.*
+import java.time.LocalDate
 
 @Entity
-class Achivement : BaseEntity() {
+class Achivement(
+    title: String,
+    description: String,
+    achivedDate: LocalDate?,
+    host: String,
+    isActive: Boolean,
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "achievement_id")
     var id: Long? = null    // ? 이란 "null 값을 허용한다."
 
+    var title: String = title
 
+    var description: String = description
+
+    var achivedDate: LocalDate? = achivedDate
+
+    var host: String = host
+
+    var isActive: Boolean = isActive
 }
