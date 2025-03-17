bcb728ba13a4088e95e68b8a97fe4d9f91fa9594
Kim Myeong Hwi <103114227+redpanda54@users.noreply.github.com>
2025-03-17 23:24:07 +0900

    인터셉터 개발

--- src/main/kotlin/com/myeonghwi/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/myeonghwi/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,7 +1,24 @@
 package com.myeonghwi.portfolio.presentation.interceptor
 
+import com.myeonghwi.portfolio.domain.entity.HttpInterface
+import com.myeonghwi.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
