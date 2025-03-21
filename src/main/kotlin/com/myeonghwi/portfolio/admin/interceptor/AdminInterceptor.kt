6ea839cf11baedb0f54ffeb3d57445eb472fa9f7
Kim Myeong Hwi <103114227+redpanda54@users.noreply.github.com>
2025-03-21 22:37:16 +0900

    공통 개발 - 인터셉터

--- src/main/kotlin/com/myeonghwi/portfolio/admin/interceptor/AdminInterceptor.kt
+++ src/main/kotlin/com/myeonghwi/portfolio/admin/interceptor/AdminInterceptor.kt
@@ -7,13 +7,35 @@ import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.ModelAndView
 
 @Component
-class AdminInterceptor:HandlerInterceptor {
+class AdminInterceptor : HandlerInterceptor {
     override fun postHandle(
         request: HttpServletRequest,
         response: HttpServletResponse,
         handler: Any,
         modelAndView: ModelAndView?
     ) {
+        val menus = listOf<MenuDTO>(
+            MenuDTO(
+                "Index", listOf<PageDTO>(
+                    PageDTO(name = "Introduction", url = "/admin/introduction"),
+                    PageDTO(name = "Link", url = "/admin/link")
+                )
+            ),
+            MenuDTO(
+                "Resume", listOf<PageDTO>(
+                    PageDTO(name = "Experience", url = "/admin/experience"),
+                    PageDTO(name = "Achievement", url = "/admin/achievement"),
+                    PageDTO(name = "Skill", url = "/admin/skill")
+                )
+            ),
+            MenuDTO(
+                "Projects", listOf<PageDTO>(
+                    PageDTO(name = "Project", url = "/admin/project"),
+                    PageDTO(name = "ProjectSkill", url = "/admin/project/skill"),
+                )
+            )
+        )
 
+        modelAndView?.model?.put("menus", menus)
     }
 }
--- src/main/kotlin/com/myeonghwi/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
+++ src/main/kotlin/com/myeonghwi/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
@@ -5,8 +5,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 class AdminInterceptorConfiguration(
     val adminInterceptor: AdminInterceptor
-):WebMvcConfigurer {
+) : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
-
+        registry.addInterceptor(adminInterceptor)
+            .addPathPatterns("/admin/**")
+            .excludePathPatterns("/assets/**", "/css/**", "/js/**", "h2**")
     }
 }
